4f1d0bb4efe98ef1de20cb1f07839fe0ca00023c
dhruvilpatel217 <160476480+dhruvilpatel217@users.noreply.github.com>
2024-04-03 09:54:59 +0530

    Update CheckSort.cpp

--- CheckSort.cpp
+++ CheckSort.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 int main()
 {
+    Data D_New;
+    D_New.ReadCSV();
+    D_New.SortData();
+    
     Data D;
-    D.ReadCSV();
-    D.SortData();
     D.ReadSorted();
     for (int i = 0; i < D.Size; i++)
     {
@@ -21,6 +23,9 @@ int main()
                 cout << "Slot " << k + 1 << " : " << setw(2) << get<0>(D.SlotTime[i][j][k]) << ":" << setw(2) << setfill('0') << get<1>(D.SlotTime[i][j][k]) << " - " << setw(2) << get<2>(D.SlotTime[i][j][k]) << ":" << setw(2) << setfill('0') << get<3>(D.SlotTime[i][j][k]) << endl;
             }
         }
+        for(int j = 0 ; j<D.DaywiseFreeTime[i].size(); j++){
+            cout<<"Day "<<j+1<<" : "<<D.DaywiseFreeTime[i][j]<<endl;
+        }
         cout << endl;
         cout << "Free min = " << D.ans1[i] << endl;
         cout << endl;
@@ -31,6 +36,9 @@ int main()
         }
         cout << endl;
         cout << "Show Time: " << endl;
+        for(int j = 0 ; j<D.DaywiseShowTime[i].size(); j++){
+            cout<<"Day "<<j+1<<" : " <<D.DaywiseShowTime[i][j]<<endl;
+        }
         for (int j = 0; j < D.ShowTime[i].size(); j++)
         {
             cout << "Day " << j + 1 << endl;
